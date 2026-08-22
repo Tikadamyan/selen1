934664561caf98ed7aee38bcef4f6cf7d093cd4f
Tikadamyan <103689514+Tikadamyan@users.noreply.github.com>
2026-08-22 23:12:34 +0400

    Add import for ChromeOptions in verjoTest

--- src/test/java/tests/verjoTest.java
+++ src/test/java/tests/verjoTest.java
@@ -1,5 +1,5 @@
 package tests;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
